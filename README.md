a55ec28a38650f243e5f6046921f74b727883a68
Petro Mozil <mozil.petryk@gmail.com>
2022-12-05 11:09:20 +0200

    Add a report skeleton and function declarations

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# discrete_maths_project
+# discrete_maths_project report
+
+## Project structure
+The project itself consists of only one file - `two_sat.py`
+
+The file consists of four functions:
+- `read_csv` - reads the csv into a graph
+- `write_csv` - writes a grph to csv
+- `colour_graph` - returns either a recoloured graph or nothing, if the graph does not exist
+
+**Because there are only three functions, @pmozil proposes that there be 
+multiple `colour_graph functions**
+
+Here's the distribution of work:
+    - `read_csv` **(author here)**
+    - `write_csv` **(author here)**
+    - `colour_graph` **(author here)**
---
+++ two_sat.py
@@ -0,0 +1,46 @@
+"""
+The two-satisfiability problem,
+used in a graph 3-colouring problem
+"""
+
+from itertools import Dict, Tuple, List
+
+def read_csv(file_path: str) -> Dict[Tuple[int], List[Tuple[int]]]:
+    """
+    Reads a csv file and returns a graph, represented by
+    a dictionary with a coloured vertice as key and a list of coloured vertices
+    as objects
+
+    Args:
+        file_path: str - path to csv
+
+    Returns:
+        Dict[Tuple[int], List[Tuple[int]]] - a graph model
+    """
+    pass
+
+def read_csv(file_path: str, graph: Dict[Tuple[int], List[Tuple[int]]]) -> None:
+    """
+    Writes dictionary with a coloured vertice as key and
+    a list of coloured vertices as objects to a csv file
+
+    Args:
+        file_path: str - path to csv
+        graph: Dict[Tuple[int], List[Tuple[int]]] - a graph model
+    """
+    pass
+
+def colour_graph(
+        graph: Dict[Tuple[int], List[Tuple[int]]]
+        ) -> Dict[Tuple[int], List[Tuple[int]]]:
+    """
+    Parses a graph and returns either a proper coloured graph or nothing
+
+    Args:
+        graph: Dict[Tuple[int], List[Tuple[int]]] - a coloured graph
+
+    Returns:
+        Dict[Tuple[int], List[Tuple[int]]] - a coloured graph
+    """
+    pass
+    pass
