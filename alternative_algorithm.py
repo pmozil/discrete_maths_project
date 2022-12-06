a76a263ba6c102ab4e9fb742152f9608ba4127b9
Petro Mozil <mozil.petryk@gmail.com>
2022-12-06 12:58:14 +0200

    Add unoptimised and untested genetic optimisation for greedy colouring

---
+++ alternative_algorithm.py
@@ -0,0 +1,124 @@
+import random as r
+from typing import Dict, Tuple, List
+from io import StringIO
+
+def _least_colour(
+        colours: List[int]
+) -> int:
+    """
+    Get the smallest int (colour) thaat is not adjacent to given vertice
+
+    Args:
+        graph: Dict[Tuple[int], List[Tuple[int]] - the graph
+        vertice: Tuple[int] - thr vertice
+
+    Returns:
+        int - the smallest colour
+    """
+    col = 0
+    while col in colours:
+        col += 1
+    return col
+    
+
+def _colouring(
+    graph: Dict[int, List[int]],
+    colours: Dic[int, int]
+    numbering: List[int]
+) -> Tuple[Dict[Tuple[int], List[Tuple[int]]], int]:
+    """
+    Greedy colouring of a given graph
+
+    Args:
+        graph: Dict[Tuple[int], int] - a weighted adjacency list
+        path: List[int] - a sequence of vertices
+        slow: bool - whether to use 2^32 as a fallback length or 2^64
+
+    Returns:
+        int - the length of a path
+    """
+    max_col = 0
+    for node in numbering:
+        cols = [colours[nd] for nd in graph[node]] + [colours[node]]
+        colours[node] = _least_colour(cols)
+        max_col = max(max_col, colours[node])
+    return {
+        (node, colours[node]):
+            [
+                (el, colours[el]) for el in graph[node]
+            ] for node in numbering
+        }, max_col
+
+
+
+def _mutate_genome(path: List[int], mutations: int = 1) -> List[int]:
+    """
+    Mutate a genome, in our case a path
+
+    Args:
+        path: List[int] - a path
+        mutations: int - the number of mutations
+
+    Returns:
+        List[int] - a new path
+    """
+    length = len(path) - 1
+    for _ in range(mutations):
+        coord1 = r.randint(0, length)
+        while (coord2 := r.randint(0, length)) == coord1:
+            pass
+        path[coord1], path[coord2] = path[coord2], path[coord1]
+    return path
+
+
+def _gen_rand_numbering(length: int) -> List[int]:
+    """
+    Generate a random path for _TSP_genetic_memo and _TSP_genetic
+
+    Args:
+        n: int - number of vertices
+
+    Returns:
+        List[int] - a random path
+    """
+    return r.sample(list(range(0, length)), length)
+
+
+def _colouring_genetic_with_memo(
+    graph: Dict[Tuple[int], List[Tuple[int]]]
+) -> List[Tuple[Dict[Tuple[int], List[Tuple[int]]], int]]:
+    """
+    Perform a genetic graph colouring optimisation algorithm on a given graph
+
+    Args:
+        graph: Dict[Tuple[int], List[Tuple[int]] - a coloured graph
+
+    Returns:
+        Tuple[Tuple[List[int], int]] - a (somewhat) optimised colouring of a graph
+    """
+    base_graph = {}
+    colours = {}
+    for node, els in graph.items():
+        base_graph[node[0]] = [el[0] for el in els]
+        colours[node[0]] = colours[node[1]]
+    length = len(vertices)
+    colourings = {}
+
+    iters = 0
+    while iters < 1024:
+        numbering = tuple(_gen_rand_numbering(length))
+        # Tuples here, gotta have sets
+        colourings[numbering] = _colouring(base_graph, colours, numbering)
+        iters += 1
+    
+    for _ in range(32):
+        new_colourings = {}
+        for numbering in colourings:
+            new_numbering = tuple(_mutate_genome(list(numbering)))
+            if new_numbering not in colourings:
+                new_colourings[new_numbering] = _colouring(base_graph, colours, new_numbering)
+
+        for cols, el in new_colourings.items():
+            colourings[colouring] = el
+
+    return list(sorted(colourings.items(), key=lambda x: x[1]))[:5]
